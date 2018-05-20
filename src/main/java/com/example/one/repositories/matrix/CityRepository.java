3e80584f929215893c61a38045ea999c86947c2c
Dimitrios Stefos <dim_stefos@outlook.com>
2018-05-20 15:50:33 +0930

    Removed the additional unnecessary database connection and any associated dependencies

--- src/main/java/com/example/one/repositories/matrix/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.one.repositories.matrix;
-
-import com.example.one.domain.City;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CityRepository extends CrudRepository<City, Long> {
-
-}
